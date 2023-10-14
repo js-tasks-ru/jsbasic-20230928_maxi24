27c6b2a612577df1e12daf9aba8759a958934adf
Maria Albayrac <maxi24@rambler.ru>
2023-10-14 22:33:07 +0300

    feat: 1-module 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  if (n===0) {
+    return 1;
+  } else {
+    let result = 1
+
+    for (let i = n; 0 < i; i--) {
+      
+      result  = result * i
+    }
+  
+    return result
+  }    
+
 }
+
